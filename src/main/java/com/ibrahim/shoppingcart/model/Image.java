9cc2efcd2cdd7539b7191feaf4086d180dbb3873
Ibrahim Mirnga <ibrahim234mohammed@gmail.com>
2025-04-07 14:32:19 +0100

    first commit

---
+++ src/main/java/com/ibrahim/shoppingcart/model/Image.java
@@ -0,0 +1,33 @@
+package com.ibrahim.shoppingcart.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String fileName;
+    private String fileType;
+
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
